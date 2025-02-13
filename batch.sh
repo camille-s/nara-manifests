4db4c66d29b6eb6dd8aaffb1a02a7cc854edd72d
camille-s <camille.seaberry@gmail.com>
2025-02-13 18:09:22 -0500

    Add metadata from full batch run

--- batch.sh
+++ batch.sh
@@ -4,16 +4,15 @@ limit=$2
 dryrun=$3
 # read rows of ids file, call python script on each id
 # if dryrun is set, add no_download flag
-if [ "$dryrun" == "dryrun" ]; then
-    flag=" -n"
-else 
-    flag=""
-fi
 while IFS= read -r row; do 
     # split by comma
     name=$(echo "$row" | cut -d',' -f1)
     id=$(echo "$row" | cut -d',' -f2)
     echo ""
     echo "Processing series '$name' with id $id"
-    python ./fetch_records.py --id "$id" --limit "$limit""$flag"
+    if [ "$dryrun" == "dryrun" ]; then
+        python ./fetch_records.py --id "$id" --limit "$limit" -n
+    else 
+        python ./fetch_records.py --id "$id" --limit "$limit"
+    fi
 done < "$ids"
